3b3e267ab8605fa663705bdf92d24b6ed497a51f
Jobs Gate <organicchemistry_01@yahoo.com.ph>
2021-03-21 17:27:58 +0800

    Update index.js to support linux

--- app/shell/index.js
+++ app/shell/index.js
@@ -1,9 +1,12 @@
 import mac from './mac'
 import windows from './windows'
+import linux from './linux'
 
 let shell = mac
 if (process.platform === 'win32') {
   shell = windows
+} else if (process.platform === 'linux') {
+  shell = linux
 }
 
 export default shell
